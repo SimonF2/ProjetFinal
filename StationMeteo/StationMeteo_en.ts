3bcc26fe0cbbee33fb5b3f19526f1cb841273a3c
Simon Fourgon <simon.fourgon@akka.eu>
2021-06-23 15:07:03 +0200

    Ajout fichiers .ts

---
+++ StationMeteo/StationMeteo_en.ts
@@ -0,0 +1,268 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>DialogOptions</name>
+    <message>
+        <location filename="dialogoptions.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="42"/>
+        <source>Format de l&apos;heure</source>
+        <translation>Hour format</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="57"/>
+        <source>Format 12h</source>
+        <translation>12h format</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="80"/>
+        <source>Format 24h</source>
+        <translation>24h format</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="103"/>
+        <source>Heure</source>
+        <translation>Hour</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="126"/>
+        <source>Police et Style</source>
+        <translation>Font and style</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="142"/>
+        <source>Langue :</source>
+        <translation>Language:</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="169"/>
+        <source>Français</source>
+        <translation>Français</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="174"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="191"/>
+        <source>Format des Températures</source>
+        <translation>Temperature unit</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="206"/>
+        <source>Celsius</source>
+        <translation>Celsius</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="229"/>
+        <source>Fahrenheit</source>
+        <translation>Fahrenheit</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="249"/>
+        <source>Mode d&apos;affichage</source>
+        <translation>Display mode</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="264"/>
+        <source>Mode Jour</source>
+        <translation>Day mode</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="287"/>
+        <source>Mode Nuit</source>
+        <translation>Night mode</translation>
+    </message>
+</context>
+<context>
+    <name>Dialoglangue</name>
+    <message>
+        <location filename="dialoglangue.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialogue</translation>
+    </message>
+    <message>
+        <location filename="dialoglangue.ui" line="26"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="dialoglangue.ui" line="48"/>
+        <source>Français</source>
+        <translation>Français</translation>
+    </message>
+    <message>
+        <location filename="dialoglangue.ui" line="53"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <source>Choix de la langue</source>
+        <translation type="vanished">Choix de la langue</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Main Window</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <source>Date</source>
+        <translation>Date</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <source>&amp;Changer de Ville</source>
+        <translation>&amp;Change city</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="239"/>
+        <source>Ville Séléctionnée : </source>
+        <translation>Selected city:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="272"/>
+        <source>Paris</source>
+        <translation>Paris</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="486"/>
+        <source>Conditions Météo en Mer:</source>
+        <translation>Weather conditions at sea</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="989"/>
+        <source>&amp;Administration</source>
+        <translation>&amp;Administration</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="998"/>
+        <source>&amp;Paramètres</source>
+        <translation>&amp;Settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1001"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1006"/>
+        <source>&amp;Quitter</source>
+        <translation>&amp;Quit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1009"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <source>Station Météo</source>
+        <translation>Weather station</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="331"/>
+        <source>%1 
+
+Conditions Météo :   
+    %2 
+Température :   
+    %3  %4
+Température Ressentie :   
+    %5  %6
+Pression atmosphérique :   
+    %7  hPa 
+Taux d&apos;Humidité :   
+    %8  %</source>
+        <translation>%1 
+
+Weather conditions:   
+    %2 
+Temperature:   
+    %3  %4
+Felt temperature:   
+    %5  %6
+Atmospheric pressure:   
+    %7  hPa 
+Humidity level:   
+    %8  %</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="525"/>
+        <source>%1 
+
+Conditions Météo :     
+    %2 
+Température :     %3 %4 
+Température Ressentie :     %5 %6  </source>
+        <translation>%1 
+
+Weather conditions:     
+    %2 
+Temperature:     %3 %4 
+Felt temperature:     %5 %6  </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="658"/>
+        <source>Conditions Météo en Mer :</source>
+        <translation>Weather conditions at sea:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="661"/>
+        <source>Température :   
+    %1  %2
+
+Pression atmosphérique :   
+    %3  hPa
+
+Taux d&apos;Humidité :   
+    %4  %</source>
+        <translation>Temperature:   
+    %1  %2
+
+Atmospheric  pressure:   
+    %3  hPa
+
+Humidity level:   
+    %4  %</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="672"/>
+        <source>Paramètres</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="690"/>
+        <source>Quitter</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="691"/>
+        <source>Vous allez quitter le programme.</source>
+        <translation>You&apos;re going to leave the application.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="692"/>
+        <source>Êtes-vous sûr?</source>
+        <translation>Are you sure?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="695"/>
+        <source>Oui</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="696"/>
+        <source>Non</source>
+        <translation>No</translation>
+    </message>
+</context>
+</TS>
---
+++ StationMeteo/StationMeteo_fr.ts
@@ -0,0 +1,268 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>DialogOptions</name>
+    <message>
+        <location filename="dialogoptions.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialogue</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="42"/>
+        <source>Format de l&apos;heure</source>
+        <translation>Format de l&apos;heure</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="57"/>
+        <source>Format 12h</source>
+        <translation>Format 12h</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="80"/>
+        <source>Format 24h</source>
+        <translation>Format 24h</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="103"/>
+        <source>Heure</source>
+        <translation>Heure</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="126"/>
+        <source>Police et Style</source>
+        <translation>Police et Style</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="142"/>
+        <source>Langue :</source>
+        <translation>Langue:</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="169"/>
+        <source>Français</source>
+        <translation>Français</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="174"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="191"/>
+        <source>Format des Températures</source>
+        <translation>Unité des températures</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="206"/>
+        <source>Celsius</source>
+        <translation>Celsius</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="229"/>
+        <source>Fahrenheit</source>
+        <translation>Fahreneit</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="249"/>
+        <source>Mode d&apos;affichage</source>
+        <translation>Mode d&apos;affichage</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="264"/>
+        <source>Mode Jour</source>
+        <translation>Mode jour</translation>
+    </message>
+    <message>
+        <location filename="dialogoptions.ui" line="287"/>
+        <source>Mode Nuit</source>
+        <translation>Mode Nuit</translation>
+    </message>
+</context>
+<context>
+    <name>Dialoglangue</name>
+    <message>
+        <location filename="dialoglangue.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialogue</translation>
+    </message>
+    <message>
+        <location filename="dialoglangue.ui" line="26"/>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialoglangue.ui" line="48"/>
+        <source>Français</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialoglangue.ui" line="53"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Choix de la langue</source>
+        <translation type="vanished">Choix de la langue</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Fenêtre principale</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <source>Date</source>
+        <translation>Date</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <source>&amp;Changer de Ville</source>
+        <translation>&amp;Changer de ville</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="239"/>
+        <source>Ville Séléctionnée : </source>
+        <translation>Ville sélectionnée</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="272"/>
+        <source>Paris</source>
+        <translation>Paris</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="486"/>
+        <source>Conditions Météo en Mer:</source>
+        <translation>Conditions météo en mer:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="989"/>
+        <source>&amp;Administration</source>
+        <translation>&amp;Administration</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="998"/>
+        <source>&amp;Paramètres</source>
+        <translation>&amp;Paramètres</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1001"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1006"/>
+        <source>&amp;Quitter</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1009"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <source>Station Météo</source>
+        <translation>Station météo </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="331"/>
+        <source>%1 
+
+Conditions Météo :   
+    %2 
+Température :   
+    %3  %4
+Température Ressentie :   
+    %5  %6
+Pression atmosphérique :   
+    %7  hPa 
+Taux d&apos;Humidité :   
+    %8  %</source>
+        <translation>%1 
+
+Conditions météo:   
+    %2 
+Température:   
+    %3  %4
+Température ressentie:   
+    %5  %6
+Pression atmosphérique:   
+    %7  hPa 
+Taux d&apos;humidité:   
+    %8  %</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="525"/>
+        <source>%1 
+
+Conditions Météo :     
+    %2 
+Température :     %3 %4 
+Température Ressentie :     %5 %6  </source>
+        <translation>%1 
+
+Conditions météo:     
+    %2 
+Température:     %3 %4 
+Température ressentie:     %5 %6  </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="658"/>
+        <source>Conditions Météo en Mer :</source>
+        <translation>Conditions météo en mer:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="661"/>
+        <source>Température :   
+    %1  %2
+
+Pression atmosphérique :   
+    %3  hPa
+
+Taux d&apos;Humidité :   
+    %4  %</source>
+        <translation>Température:   
+    %1  %2
+
+Pression atmosphérique:   
+    %3  hPa
+
+Taux d&apos;humidité:   
+    %4  %</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="672"/>
+        <source>Paramètres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="690"/>
+        <source>Quitter</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="691"/>
+        <source>Vous allez quitter le programme.</source>
+        <translation>Vous allez quitter le programme.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="692"/>
+        <source>Êtes-vous sûr?</source>
+        <translation>Êtes-vous sûr?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="695"/>
+        <source>Oui</source>
+        <translation>Oui</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="696"/>
+        <source>Non</source>
+        <translation>Non</translation>
+    </message>
+</context>
+</TS>
